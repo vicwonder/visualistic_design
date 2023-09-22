3756a2efbbf35171cae39ea1c8fa9a3cf6e72a51
vicwonder <matthewvicwonder@gmail.com>
2023-09-22 12:26:04 +0100

    fix mobile navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,35 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { FaFacebookF, FaTwitter, FaInstagram, FaBars } from "react-icons/fa";
 import { FaCircleChevronRight } from "react-icons/fa6";
 import CustomButton from "./CustomButton";
-import { useState } from "react";
 
 const Header = () => {
   const [showLink, setShowLink] = useState(false);
-  const head = useRef(null);
   const dropLinks = () => {
     setShowLink(!showLink);
   };
 
-  useEffect(() => {
-    if (showLink) {
-      head.current.classList?.add("links-mobi");
-      setTimeout(() => {
-        head.current.style.height = "250px";
-      }, 200);
-    } else {
-      head.current.style.height = "0px";
-      setTimeout(() => {
-        head.current.classList?.remove("links-mobi");
-      }, 200);
-    }
-  }, [showLink]);
   return (
     <nav className="header">
-      <div className="id">
+      <div>
         <p id="header-text">MVA-dgn</p>
       </div>
-      <div ref={head} className={`links `}>
+      <div className={`links${showLink ? " links-mobi" : ""}`}>
         <a className="link" href="about-us">
           About us
         </a>
@@ -39,10 +24,12 @@ const Header = () => {
         <a className="link" href="contacts">
           Contacts
         </a>
-        <FaFacebookF className="logos" />
-        <FaTwitter className="logos" />
-        <FaInstagram className="logos" />
-        <div className="link">
+        <div className="logo-con">
+          <FaFacebookF className="logos" />
+          <FaTwitter className="logos" />
+          <FaInstagram className="logos" />
+        </div>
+        <div>
           <CustomButton
             bg={true}
             text={"Get Started"}
@@ -50,9 +37,7 @@ const Header = () => {
           />
         </div>
       </div>
-      <div className="bar-cont">
-        <FaBars className="fa-bars" onClick={dropLinks} />
-      </div>
+      <FaBars className="fa-bars" onClick={dropLinks} />
     </nav>
   );
 };
